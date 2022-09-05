43fcb23966243547dfb276f824dcc6c00eade0dc
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-05 22:46:13 +0530

    [roshan]eual line or not

---
+++ Day8AssignmentLine/LineComparision.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8AssignmentLine
+{
+    internal class LineComparision
+    {
+        public static void CheckLengthEqualsMetho()
+        {
+            Console.WriteLine("Enter the value of x1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of p1");
+            int p1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of q1");
+            int q1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of p2");
+            int p2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of q2");
+            int q2 = Convert.ToInt32(Console.ReadLine());
+
+            double length1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+
+            double length2 = Math.Sqrt(Math.Pow((p2 - p1), 2) + Math.Pow((q2 - q1), 2));
+            Console.WriteLine("the line length 1 = "+length1);
+            Console.WriteLine("the line length 2 = "+length2);
+            if (length1.Equals(length2))
+            {
+                Console.WriteLine("The length of two lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("The length of two lines are not equal");
+            }
+        }
+    }
+}
--- Day8AssignmentLine/Program.cs
+++ Day8AssignmentLine/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("welecome line comparision");
-            Console.WriteLine("Choose a option\n 1. Length of line");
+            Console.WriteLine("Choose a option\n 1. Length of line\n2.length equal method");
             Console.WriteLine("Enter the number");
             int option = Convert.ToInt32(Console.ReadLine());
 
@@ -13,6 +13,9 @@
             {
                 case 1:
                     LengthOfLine.CalculateLengthOfLine();
+                    break;
+                    case 2:
+                        LineComparision.CheckLengthEqualsMetho();
                     break;
                 default:
                     Console.WriteLine("Choose a rigth number");
