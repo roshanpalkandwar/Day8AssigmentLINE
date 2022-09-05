5a85e480db648b4f495cd3d83a699c3eb562b705
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-05 23:13:43 +0530

    [roshan]BY using Oops concept

--- Day8AssignmentLine/Program.cs
+++ Day8AssignmentLine/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("welecome line comparision");
-            Console.WriteLine("Choose a option\n 1. Length of line\n2.length equal method\n3.compare method");
+            Console.WriteLine("Choose a option\n 1. Length of line\n2.length equal method\n3.compare method\n4.using oops concept");
             Console.WriteLine("Enter the number");
             int option = Convert.ToInt32(Console.ReadLine());
 
@@ -19,6 +19,15 @@
                     break;
                 case 3:
                     CompareMethod.CheckLengthCompare();
+                    break;
+                case 4:
+                    UsingOOPs obj=new UsingOOPs();
+                    obj.SetLength1(2, 4, 6, 8);
+                    obj.GetLength1();
+                    obj.SetLength2(2, 4, 6, 8);
+                    obj.GetLength2();
+                    obj.display1();
+                    obj.display2();
                     break;
                 default:
                     Console.WriteLine("Choose a rigth number");
