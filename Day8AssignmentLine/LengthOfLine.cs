9648f5da61bc9db1cb38090d9b3bb9277bc1977b
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-05 22:08:50 +0530

    [roshan]Length Of line

---
+++ Day8AssignmentLine/LengthOfLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8AssignmentLine
+{
+    internal class LengthOfLine
+    {
+
+        public static void CalculateLengthOfLine()
+        {
+            Console.WriteLine("Enter the value of x1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Length of line is " + length);
+
+
+        }
+    }
+}
---
+++ Day8AssignmentLine/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Day8AssignmentLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welecome line comparision");
+            Console.WriteLine("Choose a option\n 1. Length of line");
+            Console.WriteLine("Enter the number");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    LengthOfLine.CalculateLengthOfLine();
+                    break;
+                default:
+                    Console.WriteLine("Choose a rigth number");
+                    break;
+            }
+        }
+    }
+}
